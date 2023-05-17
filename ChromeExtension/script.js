cdc6ab5ec115156b86a8dbaca97cebc181bdeab5
RahulSelvakumar <112105461+RahulSelvakumar@users.noreply.github.com>
2023-05-17 19:41:00 +0530

    Added as a extension in browser

--- ChromeExtension/script.js
+++ ChromeExtension/script.js
@@ -1,12 +1,32 @@
+// chrome://extensions/
 let link=[]
 const ilink=document.getElementById("input-el")
 const button=document.getElementById("input-btn")
+const button1=document.getElementById("delete-btn")
 const ul=document.getElementById("ulel")
+
+let linkFromLocalStorage=[]
+linkFromLocalStorage=JSON.parse(localStorage.getItem("links"))
+if(linkFromLocalStorage){
+    link=linkFromLocalStorage
+    renderLink()
+}
+
 button.addEventListener("click",function() {
     link.push(ilink.value)
     ilink.value=""
+    localStorage.setItem("links",JSON.stringify(link))
+    renderLink()
+    console.log(link)
+})
+
+button1.addEventListener("dblclick",function(){
+    localStorage.clear()
+    link=[]
     renderLink()
 })
+
+
 
 function renderLink(){
     let finalLinks=""
@@ -15,4 +35,3 @@ function renderLink(){
     }
     ul.innerHTML=finalLinks
 }
-
